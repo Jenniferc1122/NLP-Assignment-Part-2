ae32c8158248e730b68ec9bc85d16fcd295d9df2
Jenniferc1122 <jenniferchee1122@gmail.com>
2025-12-22 21:39:34 +0800

    Implement sentiment analysis for gaming reviews
    Added sentiment analysis functionality with CSV upload support.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,97 @@
+import pickle
 import streamlit as st
+import cleantext
+import re
+import nltk
 
-st.title('🎈 App Name')
+#--- Streamlit UI ---
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"] {
+    background-image: url("https://images.unsplash.com/photo-1732132966168-34cf0a39b840?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D");
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+}
 
-st.write('Hello world!')
+[data-testid="stHeader"] {
+    background-color: rgba(0, 0, 0, 0);
+
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
+
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+
+stop_words = set(stopwords.words('english'))
+stemmer = PorterStemmer()
+
+def preprocess_text(text):
+    # lowercase
+    text = text.lower()
+    # remove punctuation & special characters
+    text = re.sub(r'[^a-z\s]', '', text)
+    
+    # tokenisation
+    tokens = nltk.word_tokenize(text)
+    
+    # remove stopwords & stemming
+    tokens = [stemmer.stem(word) for word in tokens if word not in stop_words]
+    
+    return " ".join(tokens)
+
+
+with open("sentiment_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+with open("tfidf.pkl", "rb") as f:
+    vectorizer = pickle.load(f)
+
+label_map = {
+    0: "Negative 😞",
+    1: "Neutral 😐",
+    2: "Positive 😊"
+}
+
+#--- Streamlit App ---
+
+st.header("**Sentiment Analysis for Gaming Review**")
+with st.expander("Input Review"):
+    user_input = st.text_area("Enter your review here:")
+    if user_input:
+        clean_text = preprocess_text(user_input)
+        vec_text = vectorizer.transform([clean_text])
+        prediction = model.predict(vec_text)[0]
+        sentiment = label_map[prediction]
+        st.success(f"Predicted Sentiment: {sentiment}")
+
+    #pre = st.text_input("Clean Review:")
+    #if pre:
+    #    st.write(cleantext.clean(pre, clean_all=False, extra_spaces=True, punct=True, lowercase=True, stopwords=True, numbers=True))
+
+with st.expander("Analyse CSV"):
+    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+    if uploaded_file is not None:
+        import pandas as pd
+        df = pd.read_csv(uploaded_file)
+        st.write("Preview of uploaded data:")
+        st.write(df.head())
+
+        if 'review_text' in df.columns:
+            df['clean_review'] = df['review_text'].apply(preprocess_text)
+            # Predict
+            preds = model.predict(
+                vectorizer.transform(df['clean_review'])
+            )
+
+            # Convert numeric labels to text
+            df['prediction'] = [label_map[p] for p in preds]
+
+            st.write("Prediction results:")
+            st.write(df[['review_text', 'prediction']].head())
+
+        else:
+            st.error("CSV must contain a 'review_text' column")
